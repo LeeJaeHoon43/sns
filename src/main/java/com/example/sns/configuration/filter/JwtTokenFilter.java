d533368b16ef8b173e6a38a546788ceba0cef3b1
LeeJaeHoon43 <ljw160791@gmail.com>
2023-04-02 14:28:37 +0900

    Jwt Token Filter 버그 수정.

--- src/main/java/com/example/sns/configuration/filter/JwtTokenFilter.java
+++ src/main/java/com/example/sns/configuration/filter/JwtTokenFilter.java
@@ -28,27 +28,30 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
-        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
         final String token;
         try {
             if (TOKEN_IN_PARAM_URLS.contains(request.getRequestURI())) {
                 log.info("Request with {} check the query param", request.getRequestURI());
                 token = request.getQueryString().split("=")[1].trim();
-            } else if (header == null || !header.startsWith("Bearer ")) {
-                log.error("Authorization Header does not start with Bearer {}", request.getRequestURI());
+            } else {
+                final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+                if (header == null || !header.startsWith("Bearer ")) {
+                    log.error("Authorization Header does not start with Bearer {}", request.getRequestURI());
+                    chain.doFilter(request, response);
+                    return;
+                }
+                token = header.split(" ")[1].trim();
+            }
+
+            if (JwtTokenUtils.isExpired(token, secretKey)){
+                log.error("Key is expired");
                 chain.doFilter(request, response);
                 return;
-            } else {
-                token = header.split(" ")[1].trim();
             }
 
             String userName = JwtTokenUtils.getUserName(token, secretKey);
             User userDetails = userService.loadUserByUserName(userName);
 
-            if (!JwtTokenUtils.validate(token, userDetails.getUsername(), secretKey)) {
-                chain.doFilter(request, response);
-                return;
-            }
             UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
                     userDetails, null,
                     userDetails.getAuthorities()
@@ -56,6 +59,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
             authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
             SecurityContextHolder.getContext().setAuthentication(authentication);
         } catch (RuntimeException e) {
+            log.error("Error occurs while validation. {}", e.toString());
             chain.doFilter(request, response);
             return;
         }
